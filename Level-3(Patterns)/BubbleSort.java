b416fc4bed6d951e11e2986f2690cbe090edc637
Ganesh <ganesh02003@gmail.com>
2023-12-18 15:02:38 +0530

    Bubble sort using recursion approach-2

--- Level-3(Patterns)/BubbleSort.java
+++ Level-3(Patterns)/BubbleSort.java
@@ -4,7 +4,7 @@ public class BubbleSort {
     public static void main(String[] args) {
         int arr[] = new int[]{5,4,3,2,1};
 
-        bubbleSort(arr,0,0,arr.length);
+        bubbleSort1(arr,arr.length-1,0);
         System.out.println(Arrays.toString(arr));
     }
     private static void bubbleSort(int []arr,int r,int c,int n){
@@ -19,6 +19,20 @@ public class BubbleSort {
             bubbleSort(arr, r, c+1, n);
         }else{
             bubbleSort(arr, r+1, 0, n);
+        }
+    }
+    private static void bubbleSort1(int []arr,int r,int c){
+        if(r==0) return;
+
+        if(c<r){
+            if(arr[c]>arr[c+1]){
+                int temp = arr[c];
+                arr[c] = arr[c+1];
+                arr[c+1] = temp;
+            }
+            bubbleSort1(arr, r, c+1);
+        }else{
+            bubbleSort1(arr, r-1, 0);
         }
     }
 }
