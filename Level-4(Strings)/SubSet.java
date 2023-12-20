ce3c7a62d3c7e5816b5c1565fe397e3cead9c9d7
Ganesh <ganesh02003@gmail.com>
2023-12-20 15:17:45 +0530

    returning the list without using the arguments

--- Level-4(Strings)/SubSet.java
+++ Level-4(Strings)/SubSet.java
@@ -3,11 +3,12 @@ import java.util.List;
 
 public class SubSet {
     public static void main(String[] args) {
-        String s = "abc";
-        printSubset("",s);
-        List<String> ans = new ArrayList<>();
-        makeSubsetList(s,"",ans);
-        System.out.println(ans);
+        String s = "abcdefghij";
+        // printSubset("",s);
+        // List<String> ans = new ArrayList<>();
+        // makeSubsetList(s,"",ans);
+        // System.out.println(ans);
+        System.out.println(makeSubsetList1(s,""));
     }
     private static void printSubset(String processed,String unprocessed){
         if(unprocessed.isEmpty()){
@@ -21,7 +22,7 @@ public class SubSet {
         printSubset(processed, unprocessed.substring(1));
     }
 
-    
+    //passing list as argument and adding the subsets
     private static void makeSubsetList(String unprocessed,String processed,List<String> ans){
         if(unprocessed.isEmpty()){
             ans.add(processed.toString());
@@ -31,5 +32,22 @@ public class SubSet {
         char c = unprocessed.charAt(0);
         makeSubsetList(unprocessed.substring(1),processed+c,ans);
         makeSubsetList(unprocessed.substring(1), processed, ans);
+    }
+
+    //returning the list without using the arguments
+    private static ArrayList<String> makeSubsetList1(String unprocessed,String processed){
+        if(unprocessed.isEmpty()){
+            ArrayList<String> ans  = new ArrayList<>();
+            ans.add(processed);
+            return ans;
+        }
+
+        char c = unprocessed.charAt(0);
+        ArrayList<String> left = makeSubsetList1(unprocessed.substring(1), processed+c);
+        ArrayList<String> right = makeSubsetList1(unprocessed.substring(1), processed);
+
+        left.addAll(right);
+
+        return left;
     }
 }
