4795bda1318bfd6ad916a9503227bf0fcacdee1c
Ganesh <ganesh02003@gmail.com>
2023-12-20 09:28:15 +0530

    skip string approach 2

--- Level-4(Strings)/SkipApple.java
+++ Level-4(Strings)/SkipApple.java
@@ -3,6 +3,7 @@ public class SkipApple {
         String s = "baxappledshf";
         StringBuilder ans = new StringBuilder();
         skipApple(s,ans);
+        System.out.println(skipApple1(s));
     }
     private static void skipApple(String s,StringBuilder sb){
         if(s.isEmpty()){
@@ -15,6 +16,17 @@ public class SkipApple {
         // add else, if you does not have return type in the function for if the if-else condition
         else{
             skipApple(s.substring(1), sb.append(s.charAt(0)));
+        }
+    }
+    private static String skipApple1(String s){
+        if(s.isEmpty()){
+            return "";
+        }
+        char c = s.charAt(0);
+        if(s.startsWith("apple")){
+            return skipApple1(s.substring(5));
+        }else{
+            return c + skipApple1(s.substring(1));
         }
     }
 }
