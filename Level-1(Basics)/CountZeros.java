8418bfc8b184667a84e71c91142158e85c0b6d37
Ganesh <ganesh02003@gmail.com>
2023-12-20 15:28:41 +0530

    returning the count without using argument

--- Level-1(Basics)/CountZeros.java
+++ Level-1(Basics)/CountZeros.java
@@ -1,7 +1,7 @@
 public class CountZeros {
     public static void main(String[] args) {
         int  n = 1030000050;
-        System.out.println(countZero(n,0));
+        System.out.println(countZero1(n));
     }
     private static int countZero(int n,int count){
         //base condition
@@ -11,5 +11,13 @@ public class CountZeros {
 
         return countZero(n/10, count);
     }
-    
+
+    //returning the count value without using argument
+    private static int countZero1(int n){
+        int count = 0;
+        if(n == 0) return count;
+        
+        if(n%10 == 0) count ++;
+        return count+countZero1(n/=10);
+    }
 }
