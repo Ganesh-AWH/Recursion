ba6939a4544877f20efddb7a2b597b44ec883b93
Ganesh <ganesh02003@gmail.com>
2023-12-21 09:29:00 +0530

    Subset finding Iterative approach

--- Level-4(Strings)/SubsetIterative.java
+++ Level-4(Strings)/SubsetIterative.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SubsetIterative {
     public static void main(String[] args) {
-        int []arr = {1,2,3};
+        int []arr = {1,2,2};
 
-        List<List<Integer>> ans = subset(arr);
+        List<List<Integer>> ans = subsetDuplicate(arr);
         for(List<Integer> i:ans){
             System.out.println(i);
         }
@@ -22,6 +23,29 @@ public class SubsetIterative {
                 outer.add(inner);
             }
         }
+        return outer;
+    }
+    private static List<List<Integer>> subsetDuplicate(int []arr){
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        //sorting is applied to make the duplicate element adjacent
+        Arrays.sort(arr);
+        int start = 0;
+        int end = 0;
+        for(int i=0;i<arr.length;i++){
+            start = 0;
+            if(i>0 && arr[i] == arr[i-1]){
+                start =  end + 1;
+            }
+            end = outer.size()-1;
+            int n = outer.size();
+            for(int j=start;j<n;j++){
+                List<Integer> inner = new ArrayList<>(outer.get(j));
+                inner.add(arr[i]);
+                outer.add(inner);
+            }
+        }
+
         return outer;
     }
 }
