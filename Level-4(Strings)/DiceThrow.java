571fdc273ab499721c3110c40dd09863aa394593
Ganesh <ganesh02003@gmail.com>
2023-12-22 12:11:05 +0530

    dice throw making list

--- Level-4(Strings)/DiceThrow.java
+++ Level-4(Strings)/DiceThrow.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiceThrow {
     public static void main(String[] args) {
         int n = 3;
         dicePrint("",n);
+        List<String> ans = new ArrayList<>();
+        makeList("", n, ans);
+        System.out.println(ans);
     }    
     private static void dicePrint(String processed,int target){
         if(target == 0){
@@ -12,5 +18,15 @@ public class DiceThrow {
         for(int i=1;i<=6 && i<=target;i++){
             dicePrint(processed+i, target-i);
         }
+    }
+    private static void makeList(String processed,int target,List<String> ans){
+        if(target==0){
+            ans.add(processed);
+        }
+
+        for(int i=1;i<=6 && i<=target;i++){
+            makeList(processed+i, target-i, ans);
+        }
+
     }
 }
